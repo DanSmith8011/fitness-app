2557da83ec78d7eec35dcc7d9219597508dcb88b
DanSmith8011 <dansmithdan7@aol.co.uk>
2026-03-12 21:54:34 +0000

    set up routing and page components

---
+++ frontend/src/pages/WorkoutHistory.jsx
@@ -0,0 +1,9 @@
+function WorkoutHistory() {
+    return (
+        <div>
+            <h1>Workout History</h1>
+        </div>
+    )
+}
+
+export default WorkoutHistory
---
+++ frontend/src/pages/WorkoutLog.jsx
@@ -0,0 +1,9 @@
+function WorkoutLog() {
+    return (
+        <div>
+            <h1>Workout Log</h1>
+        </div>
+    )
+}
+
+export default WorkoutLog
